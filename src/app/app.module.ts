0b34c14d9acee1de21a175a711f5c063acccc2bd
LorenaChulde <lchude@yavirac.edu.ec>
2023-05-12 16:43:56 -0500

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -14,7 +16,9 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
         BrowserModule,
         SharedModule,
-        AppRoutingModule
+        AppRoutingModule,
+        PagesModule,
+        HttpClientModule
     ]
 })
 export class AppModule { }
