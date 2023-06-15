4d77d27aeddde8c8a1d45aab6bec020f9ba3bb88
LorenaChulde <lchude@yavirac.edu.ec>
2023-06-15 08:07:05 -0500

    templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
@@ -14,7 +15,8 @@ import { PagesModule } from './pages/pages.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        PagesModule
+        PagesModule,
+        HttpClientModule
     ]
 })
 export class AppModule { }
